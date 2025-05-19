3ce7e72fb1ed5604af5ea7039054cd0f723ec7f3
Narsing <pendharkarnarsing@gmail.com>
2025-05-19 18:54:25 +0530

    up

--- UManage/UManage/src/main/java/org/umanage/controller/AdminController.java
+++ UManage/UManage/src/main/java/org/umanage/controller/AdminController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,66 +22,62 @@ import org.umanage.entity.AppUser;
 import org.umanage.exception.UserAlreadyExistsException;
 import org.umanage.service.UserdataService;
 import org.umanage.util.EmailService;
-
 import com.itextpdf.text.DocumentException;
-
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
+	@Autowired
+	private UserdataService userService;
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	@Autowired
+	private ModelMapper modelMapper;
+	@Autowired
+	private EmailService emailService;
 
-    @Autowired private UserdataService userService;
-    @Autowired private BCryptPasswordEncoder passwordEncoder;
-    @Autowired private ModelMapper modelMapper;
-    @Autowired private EmailService emailService;
+	@PostMapping("/register")
+	public ResponseEntity<?> newUser(@RequestBody @Valid AppUser newUser)
+			throws MessagingException, IOException, DocumentException {
+		if (newUser.getId() != null) {
+			return ResponseEntity.badRequest().body("Don't send user ID!");
+		}
+		if (userService.emailExist(newUser.getEmail())) {
+			throw new UserAlreadyExistsException("User already registered with email: " + newUser.getEmail());
+		}
+		newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+		AppUser savedUser = userService.register(newUser);
+		//emailService.sendWelcomeMailWithPdf("narsingpen@gmail.com", savedUser.getName());
+		return ResponseEntity.status(HttpStatus.CREATED).body(modelMapper.map(savedUser, AppUserResponse.class));
+	}
 
-    @PostMapping("/register")
-    public ResponseEntity<?> newUser(@RequestBody @Valid AppUser newUser) throws MessagingException, IOException, DocumentException {
-        if (newUser.getId() != null) {
-            return ResponseEntity.badRequest().body("Don't send user ID!");
-        }
+	@GetMapping("/list")
+	public List<AppUserResponse> getAllUsers() {
+		return userService.getAllUsers()
+				.stream()
+				.map(user -> modelMapper.map(user, AppUserResponse.class)).toList();
+	}
 
-        if (userService.emailExist(newUser.getEmail())) {
-            throw new UserAlreadyExistsException("User already registered with email: " + newUser.getEmail());
-        }
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updateUser(@RequestBody @Valid AppUser userdata, @PathVariable Long id) {
+		userdata.setPassword(passwordEncoder.encode(userdata.getPassword()));
+		AppUser updated = userService.updateUser(userdata, id);
+		return ResponseEntity.ok(modelMapper.map(updated, AppUserResponse.class));
+	}
 
-        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
-        AppUser savedUser = userService.register(newUser);
-        emailService.sendWelcomeMailWithPdf("narsingpen@gmail.com", savedUser.getName());
+	// @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Map<String, Object>> deleteUserById(@PathVariable Long id) {
+		userService.deleteUser(id);
+		Map<String, Object> response = new HashMap<>();
+		response.put("message", "User deleted successfully");
+		return ResponseEntity.ok(response);
+	}
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(modelMapper.map(savedUser, AppUserResponse.class));
-    }
-
-    @GetMapping("/list")
-    public List<AppUserResponse> getAllUsers() {
-        return userService.getAllUsers().stream()
-                .map(user -> modelMapper.map(user, AppUserResponse.class))
-                .toList();
-    }
-
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateUser(@RequestBody @Valid AppUser userdata, @PathVariable Long id) {
-        userdata.setPassword(passwordEncoder.encode(userdata.getPassword()));
-        AppUser updated = userService.updateUser(userdata, id);
-        return ResponseEntity.ok(modelMapper.map(updated, AppUserResponse.class));
-    }
-
-   // @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Map<String, Object>> deleteUserById(@PathVariable Long id) {
-        userService.deleteUser(id);
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", "User deleted successfully");
-        return ResponseEntity.ok(response);
-    }
-
-
-    @GetMapping("/{id}")
-    public AppUserResponse getUserById(@PathVariable Long id) {
-        return modelMapper.map(userService.getUserById(id), AppUserResponse.class);
-    }
+	@GetMapping("/{id}")
+	public AppUserResponse getUserById(@PathVariable Long id) {
+		return modelMapper.map(userService.getUserById(id), AppUserResponse.class);
+	}
 }
--- UManage/UManage/src/main/java/org/umanage/dto/AppUserResponse.java
+++ UManage/UManage/src/main/java/org/umanage/dto/AppUserResponse.java
@@ -1,13 +1,18 @@
 package org.umanage.dto;
- import lombok.Data;
- 
- @Data
- public class AppUserResponse {
- 	    private Long id;
- 	    private String name;
- 	    private String email;
- 	    private String roles;
- 	    private boolean enabled ;
- 	 
-		
- }
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class AppUserResponse {
+	private Long id;
+	private String name;
+	private String email;
+	private String roles;
+	private boolean enabled;
+	private String createdBy;
+	private String modifiedBy;
+	private LocalDateTime createdAt;
+	private LocalDateTime modifiedAt;
+}
